6b980fb540de93d0f290439eee83812b370e016d
danielkim <37679062+eoqkrskfk94@users.noreply.github.com>
2021-12-06 23:12:51 +0900

    [Add] test suite

--- app/src/androidTest/java/com/mj/espressouitestexample/MainActivityTest.kt
+++ app/src/androidTest/java/com/mj/espressouitestexample/MainActivityTest.kt
@@ -2,6 +2,8 @@ package com.mj.espressouitestexample
 
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
@@ -15,39 +17,73 @@ class MainActivityTest {
 
 
     @Test
-    fun test_isActivityInView() {
+    fun test_navSecondaryActivity() {
         //launch activity with espresso
         val activityScenario = ActivityScenario.launch(MainActivity::class.java)
 
-        //test if an activity is in view
+        // test next button click
+        onView(withId(R.id.button_next_activity)).perform(click())
+
+        // test if secondary activity is in view
+        onView(withId(R.id.secondary)).check(matches(isDisplayed()))
+
+    }
+
+    @Test
+    fun test_backPress_toMainActivity() {
+        //launch activity with espresso
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+
+        // test next button click
+        onView(withId(R.id.button_next_activity)).perform(click())
+
+        // test if secondary activity is in view
+        onView(withId(R.id.secondary)).check(matches(isDisplayed()))
+
+        // test back button click
+        onView(withId(R.id.button_back)).perform(click())
+
+        // pressBack() // method 2
+
+        // test if secondary activity is in view
         onView(withId(R.id.main)).check(matches(isDisplayed()))
     }
 
-    @Test
-    fun test_visibility_title_nextButton() {
-        //launch activity with espresso
-        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
 
-        //test if text is in view
-        onView(withId(R.id.activity_main_title))
-            .check(matches(isDisplayed()))
-
-        //test if button is in view
-        onView(withId(R.id.button_next_activity))
-            .check(matches(isDisplayed()))
-
-        //test if button is visible
-        onView(withId(R.id.button_next_activity))
-            .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
-    }
-
-    @Test
-    fun test_isTitleTextDisplayed() {
-        //launch activity with espresso
-        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-
-        //test if text is a specific text
-        onView(withId(R.id.activity_main_title))
-            .check(matches(withText(R.string.text_mainactivity)))
-    }
+//    @Test
+//    fun test_isActivityInView() {
+//        //launch activity with espresso
+//        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+//
+//        //test if an activity is in view
+//        onView(withId(R.id.main)).check(matches(isDisplayed()))
+//    }
+//
+//    @Test
+//    fun test_visibility_title_nextButton() {
+//        //launch activity with espresso
+//        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+//
+//        //test if text is in view
+//        onView(withId(R.id.activity_main_title))
+//            .check(matches(isDisplayed()))
+//
+//        //test if button is in view
+//        onView(withId(R.id.button_next_activity))
+//            .check(matches(isDisplayed()))
+//
+//        //test if button is visible
+//        onView(withId(R.id.button_next_activity))
+//            .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+//    }
+//
+//    @Test
+//    fun test_isTitleTextDisplayed() {
+//        //launch activity with espresso
+//        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+//
+//        //test if text is a specific text
+//        onView(withId(R.id.activity_main_title))
+//            .check(matches(withText(R.string.text_mainactivity)))
+//    }
 }
