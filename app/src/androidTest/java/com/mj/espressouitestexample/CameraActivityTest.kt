3d736de4331e0eb7e198d611b7cbaa22415951fa
danielkim <37679062+eoqkrskfk94@users.noreply.github.com>
2021-12-12 17:47:00 +0900

    [Add] ImageViews, Drawable Matchers, Bitmaps test

--- app/src/androidTest/java/com/mj/espressouitestexample/CameraActivityTest.kt
+++ app/src/androidTest/java/com/mj/espressouitestexample/CameraActivityTest.kt
@@ -1,34 +1,59 @@
-//package com.mj.espressouitestexample
-//
-//import android.app.Instrumentation
-//import android.graphics.BitmapFactory
-//import androidx.test.espresso.intent.rule.IntentsTestRule
-//import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
-//import org.junit.Assert.*
-//import org.junit.Rule
-//import org.junit.runner.RunWith
-//
-//@RunWith(AndroidJUnit4ClassRunner::class)
-//class CameraActivityTest {
-//
-//    @get: Rule
-//    val intentsTestRule = IntentsTestRule(CameraActivity::class.java)
-//
-//    @Test
-//    fun test_cameraIntent_isBitmapSetToImage() {
-//
-//        // given
-//        val activityResult = createImageCaptureActivityResultStub()
-//    }
-//
-//
-//    private fun createImageCaptureActivityResultStub(): ActivityResult? {
-//        val bundle = Bundle()
-//        bundle.putParcelable(
-//            KEY_IMAGE_DATA,
-//            BitmapFactory.decodeResource(
-//                instru
-//            )
-//        )
-//    }
-//}
+package com.mj.espressouitestexample
+
+import android.app.Activity
+import android.app.Instrumentation
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.provider.MediaStore
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents.intending
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import com.mj.espressouitestexample.ImageViewHasDrawableMatcher.hasDrawable
+import org.hamcrest.CoreMatchers.not
+import org.hamcrest.Matcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class CameraActivityTest {
+
+    @get: Rule
+    val intentsTestRule = IntentsTestRule(CameraActivity::class.java)
+
+    @Test
+    fun test_cameraIntent_isBitmapSetToImage() {
+
+        // given
+        val activityResult = createImageCaptureActivityResultStub()
+        val expectedIntent: Matcher<Intent> = hasAction(MediaStore.ACTION_IMAGE_CAPTURE)
+        intending(expectedIntent).respondWith(activityResult)
+
+        // execute and verity
+        onView(withId(R.id.image)).check(matches(not(hasDrawable())))
+        onView(withId(R.id.button_launch_camera)).perform(click())
+        intending(expectedIntent)
+        onView(withId(R.id.image)).check(matches(hasDrawable()))
+    }
+
+
+    private fun createImageCaptureActivityResultStub(): Instrumentation.ActivityResult {
+        val bundle = Bundle()
+        bundle.putParcelable(
+            KEY_IMAGE_DATA,
+            BitmapFactory.decodeResource(
+                intentsTestRule.activity.resources,
+                R.drawable.ic_launcher_background
+            )
+        )
+        val resultData = Intent()
+        resultData.putExtras(bundle)
+        return Instrumentation.ActivityResult(Activity.RESULT_OK, resultData)
+    }
+}
